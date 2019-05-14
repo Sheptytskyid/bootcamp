6dde53509e12876e07edcab0be9f4b6e914512d3
Denys Sheptytskyi <neoxesis@ukr.net>
2019-05-14 09:34:37 +0200

    add homework task 2

---
+++ src/main/java/module2/homework/ArrayDoubleEndStack.java
@@ -0,0 +1,25 @@
+package module2.homework;
+
+public class ArrayDoubleEndStack {
+
+    int[] doubleEndStack;
+
+    //[______16,0,15_______}
+    //[_, _, _,| _, _, _]
+
+    void addLeft() {
+
+    }
+
+    void addRight() {
+
+    }
+
+    int removeLeft() {
+        return 0;
+    }
+
+    int removeRight() {
+        return 0;
+    }
+}
