c70fdb4e4d44b81f7a54c1e3efa976d076857c65
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-03 09:18:41 +0200

    Create MergeSort.java

---
+++ src/main/java/module3/classwork/MergeSort.java
@@ -0,0 +1,17 @@
+package module3.classwork;
+
+public class MergeSort {
+    //
+    //input [1,3,5,7,9,    2,4,6,8,10,12,14,16]
+    //start 0
+    //mid = beginning (position) of second sorted part
+    //end = ending position of the second sorted part
+
+    void merge(int[] array, int start, int mid, int end) {
+        int[] left; //[1,3,5,7,56]
+        int[] rigth; //[2,4,6,8,10,12,14,16]
+
+
+        //1,2,3,4,5,6,7,8,9,10,12,14,16
+    }
+}
