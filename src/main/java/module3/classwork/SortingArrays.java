a528b00fb3aed4fb9c92af806ac263de82aff4d9
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-03 09:19:27 +0200

    Create SortingArrays.java

---
+++ src/main/java/module3/classwork/SortingArrays.java
@@ -0,0 +1,15 @@
+package module3.classwork;
+
+public class SortingArrays {
+
+    public static void main(String[] args) {
+        int[] unsorted = {5,8,1,9,3,7,4,1,2};
+
+        int[] sorted = bubbleSort(unsorted);
+    }
+
+    private static int[] bubbleSort(int[] unsorted) {
+
+        return null;
+    }
+}
