77e7d4751427492cbf3e5b1c4cdcde9caf510a53
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-03 09:18:56 +0200

    Create OperationsWithLoops.java

---
+++ src/main/java/module3/classwork/OperationsWithLoops.java
@@ -0,0 +1,26 @@
+package module3.classwork;
+
+public class OperationsWithLoops {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 6, 8, 9, 10};
+        printEvens(array);
+        reverseArray(array);
+
+    }
+
+    static void printEvens(int[] array) {
+        System.out.println("Even numbers:");
+
+    }
+
+    static void reverseArray(int[] array) {
+        System.out.println("reversed array:");
+
+    }
+
+    static void sumOfArrayElements(int[] array) {
+        System.out.println("Sum of elements in the array:");
+
+    }
+}
