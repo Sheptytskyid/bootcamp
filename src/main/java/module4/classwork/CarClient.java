dcd06c976e91e1ea739debd89c4bcd81b2de4c16
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-11 09:40:01 +0200

    Create CarClient.java

---
+++ src/main/java/module4/classwork/CarClient.java
@@ -0,0 +1,12 @@
+package module4.classwork;
+
+public class CarClient {
+
+    public static void main(String[] args) {
+        Car maluch = new Car();
+        Car polonez = new Car("white", 50, 50);
+
+        System.out.println(maluch);
+        System.out.println(polonez);
+    }
+}
