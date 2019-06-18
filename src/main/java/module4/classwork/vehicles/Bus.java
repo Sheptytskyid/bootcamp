a4b48dca6402b3ad5c861924067c5a1586870e2d
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-18 09:27:29 +0200

    Create Bus.java

---
+++ src/main/java/module4/classwork/vehicles/Bus.java
@@ -0,0 +1,15 @@
+package module4.classwork.vehicles;
+
+public class Bus extends Vehicle{
+
+    int passengerSeats;
+
+    Bus(int maxSpeed, int horsepower, String model, String vehicleName, int passengerSeats) {
+        super(maxSpeed, horsepower, model, vehicleName);
+        this.passengerSeats = passengerSeats;
+    }
+
+    void printUniqueProperty() {
+        System.out.println("I have passenger seats: " + passengerSeats);
+    }
+}
