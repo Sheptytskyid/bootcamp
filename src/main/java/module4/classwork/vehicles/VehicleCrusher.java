42d217bdcc576bb585b1fd4c3f96ec8a10576e42
Denys Sheptytskyi <neoxesis@ukr.net>
2019-06-18 09:29:31 +0200

    Create VehicleCrusher.java

---
+++ src/main/java/module4/classwork/vehicles/VehicleCrusher.java
@@ -0,0 +1,9 @@
+package module4.classwork.vehicles;
+
+public class VehicleCrusher {
+
+    void destroyVehicle(Vehicle vehicle) {
+        System.out.println(vehicle.vehicleName + " destroyed: " + vehicle.model);
+    }
+
+}
