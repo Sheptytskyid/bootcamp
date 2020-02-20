ba69169188074f86b333c13c8ced6a3a34b7b440
Denys Sheptytskyi <neoxesis@ukr.net>
2020-02-20 08:44:42 +0100

    Update FirstNonRepeatingCharInArray.java

--- src/main/java/algorithms/FirstNonRepeatingCharInArray.java
+++ src/main/java/algorithms/FirstNonRepeatingCharInArray.java
@@ -18,13 +18,20 @@ public class FirstNonRepeatingCharInArray {
         System.out.println(firstNonRepeatingChar(text4));
     }
 
+    /*
+    * Traverse string twice: first create a counts map <Char, number of occurrences>
+    * then traverse string and for each char check if its count in the map is 1
+    * return the first char with 1 occurrence
+    */
     public static int firstNonRepeatingChar(String text) {
         if (text == null || text.isEmpty()) {
             return -1;
         }
         Map<Character, Integer> count = new HashMap<>();
         text = text.toLowerCase();
+        //1st traversal fill the counts map
         text.chars().forEach(c -> count.merge((char) c, 1, (o, n) -> ++o));
+        //2nd traversal find the first char in map with value 1
         for (int i = 0; i < text.length(); i++) {
             if (count.get(text.charAt(i)) == 1) {
                 return i;
